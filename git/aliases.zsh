29409cbc80bff3376d7180e3d08bc30d2cf0dc33
Timotej Kovacka <tkovacka@cisco.com>
2024-09-23 11:28:28 +0100

    add gcb command for git

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -17,3 +17,4 @@ alias gswc='gswc = git switch --create'
 
 alias gb='git branch --sort=-committerdate | fzf --header Checkout | xargs git checkout'
 alias gcleanup="git fetch -p ; git branch -r | awk '{print $1}' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print $1}' | xargs git branch -d"
+alias gcb='git branch --sort=-committerdate | fzf --header Checkout | xargs git checkout'
