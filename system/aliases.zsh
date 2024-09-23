4aef1de9a843260e5e7f9d67a5d988131c5dcc30
TimotejKovacka <timo.kovacka@gmail.com>
2024-09-23 12:00:01 +0100

    update ls alias

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,10 +1,18 @@
 # grc overides for ls
 #   Made possible through contributions from generous benefactors like
 #   `brew install coreutils`
-if $(gls &>/dev/null)
+# Check if gls (GNU ls) is available
+if command -v gls &> /dev/null
 then
+  # gls is available, use it
   alias ls="gls -F --color"
   alias l="gls -lAh --color"
   alias ll="gls -l --color"
   alias la='gls -A --color'
+else
+  # gls is not available, use the default ls
+  alias ls="ls -F --color"
+  alias l="ls -lAh --color"
+  alias ll="ls -l --color"
+  alias la='ls -A --color'
 fi
